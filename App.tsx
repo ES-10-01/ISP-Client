4670e8a023096bd7a408084e47cbb86b3228831b
Andrey Deriabin <aderiabin.98@gmail.com>
2020-11-23 17:10:21 +0300

    Added react-redux and connected Redux in App.tsx.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import reducer from './data/Reducers';
+
+const store = createStore(reducer);
 
 export default function App() {
     const isLoadingComplete = useCachedResources();
@@ -17,7 +22,9 @@ export default function App() {
         return (
             <SafeAreaProvider>
                 <PaperProvider>
-                    <Navigation colorScheme={colorScheme} />
+                    <Provider store={store}>
+                        <Navigation colorScheme={colorScheme} />
+                    </Provider>
                 </PaperProvider>
                 <StatusBar />
             </SafeAreaProvider>
